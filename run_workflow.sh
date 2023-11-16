6b103f8f05e5416a9ed6f11cba29ed4fb14ee1fa
juldpnt <jules.dupont@supagro.fr>
2023-11-16 22:18:19 +0100

    feat: add workflow runner & correct typo snakefile

---
+++ run_workflow.sh
@@ -0,0 +1,17 @@
+# ----------- Snakemake workflow for running the analysis
+
+# Modify this variable to change the number of cores used by snakemake
+cores=4
+
+# Message to the user
+echo
+echo " ----------- Reprohackathon 2023 : groupe 8 -----------"
+echo
+echo "Snakemake AND Apptainer should be installed before running this script"
+echo "The default number of cores is $cores, but can be changed in the script"
+echo
+echo " ------------------------------------------------------"
+echo
+
+# Run the workflow
+snakemake workflow/Snakefile --cores $cores -r -p --use-singularity
