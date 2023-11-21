a841f939b3da8d091275e28f12ec09cf689774f0
JulienPietra <144248552+JulienPietra@users.noreply.github.com>
2023-11-21 14:36:52 +0100

    Add files via upload

---
+++ workflow/scripts/deseq_with_kegg_21_11_2023.R
@@ -0,0 +1,66 @@
+
+setwd("C:/Users/User/Documents/HACKATHON/scripts")
+
+# ------------- Preprocess data
+subreadCounts <- read.table("counts_nous.txt", header = TRUE, sep = '\t', row.names = 1)
+new_colnames <- c("SRR10379721",
+                  "SRR10379722",
+                  "SRR10379723",
+                  "SRR10379724",
+                  "SRR10379725",
+                  "SRR10379726")
+
+# Change column names by accession number
+colnames(subreadCounts)[(ncol(subreadCounts)-5):ncol(subreadCounts)] <- new_colnames
+
+# Remove first 6 columns (chr, start, end, strand, length, name)
+subreadCounts <- subreadCounts[,7:ncol(subreadCounts)]
+
+# Convert to matrix
+subreadCounts <- as.matrix(subreadCounts)
+
+# Set up the condition vector
+(condition <- factor(c(rep("exp", 3), rep("temoin", 3))))
+
+kegg <- read.table("kegg.txt", header = FALSE, sep = "\t")
+
+kegg <- kegg[,2]
+
+kegg <- sub("sao:", "gene-", kegg)
+
+subreadCounts <- subreadCounts[c(kegg),]
+
+# ------------- Construct DESeq object
+
+library(DESeq2)
+
+(subreadData <- data.frame(row.names=colnames(subreadCounts), condition))
+dataSet <- DESeqDataSetFromMatrix(countData = subreadCounts, colData = subreadData, design = ~ condition)
+
+
+
+
+
+
+# analyse d'expression différentielle sur notre dds
+
+dataSet <- DESeq(dataSet)
+
+# formatage des résultats sous forme de DataFrame
+
+resultats <- results(dataSet)
+
+rld <- rlogTransformation(dataSet)
+
+resultats$padj <- p.adjust(resultats$pvalue, method = "BH")
+
+de_genes <- subset(resultats, padj < 0.05)
+
+# exportation des résultats sous forme de MA-plot
+
+plotMA(resultats, ylim = c(-6,6))
+plotPCA(rld, intgroup="condition")
+
+library("EnhancedVolcano")
+
+EnhancedVolcano(resultats,lab = rownames(resultats), x = 'log2FoldChange', y = 'pvalue')
